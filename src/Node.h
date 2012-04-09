b1edc21a09eb9d92ae3ae3284993d7154161acc8
jackhall <jackwhall7@gmail.com>
2012-04-09 18:48:45 -0500

    fixed stupid semicolon mistake

--- src/Node.h
+++ src/Node.h
@@ -113,8 +113,8 @@ namespace ben {
 		bool contains_input(const unsigned int nOrigin) const; 
 		bool contains_output(const unsigned int nTarget) const;
 		
-		unsigned int size_inputs() { return inputs.size() }
-		unsigned int size_outputs() { return outputs.size() }
+		unsigned int size_inputs() { return inputs.size(); }
+		unsigned int size_outputs() { return outputs.size(); }
 	
 		class input_port { //part bidirectional iterator, part stream object
 		private:
