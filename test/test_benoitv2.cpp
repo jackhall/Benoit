8239861e413f8cf72dfa96c7d2481c8fcc07e72a
jackhall <jackwhall7@gmail.com>
2012-09-29 23:21:24 -0500

    housekeeping - more needed

--- test/test_benoitv2.cpp
+++ test/test_benoitv2.cpp
@@ -1,5 +1,5 @@
 /*
-	Eiffel: the simulation of a self-organizing network of trusses
+	Benoit: distributed graph and space data structures
 	Copyright (C) 2012  John Wendell Hall
 
 	This program is free software: you can redistribute it and/or modify
