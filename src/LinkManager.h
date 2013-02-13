bf6498805c42f60fea37a5de121bbf05fd75d0f1
jackhall <jackwhall7@gmail.com>
2013-02-13 13:16:16 -0600

    specialization trick figured out

--- src/LinkManager.h
+++ src/LinkManager.h
@@ -22,27 +22,37 @@
 */
 
 namespace ben {
+
+	template<typename... args> struct ConstructionTypes {};
+
+
+	template<typename P, typename... ARGS>
+	struct LinkManagerHelper {
+		static_assert(!std::is_same<typename P::construction_types, Args...>::value, 
+				"LinkManagerHelper needs an instance of ConstructionTypes; use LinkManager");
+	};
+
 	
-	template<typename P>
-	struct LinkManager { 
+	template<typename P, typename... ARGS>
+	struct LinkManagerHelper< P, ConstructionTypes<ARGS...> > { 
 	public:
 		typedef P link_type;
-		typedef LinkManager<typename link_type::complement_type> complement_type;
+		typedef typename P::id_type id_type;
+		typedef LinkManagerHelper<typename link_type::complement_type> complement_type;
 		typedef typename std::vector<link_type>::iterator iterator; 
 		
 	private:
-		typedef LinkManager self_type;
+		typedef LinkManagerHelper self_type;
 		std::vector<link_type> links;
 		void clean_up(const id_type address); 
 	
 	public:
 		iterator find(const id_type address); 
 
-		template<typename... Args>	
-		bool add(const id_type address, const Args... args); 
+		bool add(const id_type address, const ARGS... args); 
 		bool clone(const self_type& other); 
-		void remove(const iterator iter);
-		void remove(const id_type address);
+		void remove(const iterator iter, complement_type& other);
+		void remove(const id_type address, complement_type& other);
 		void clear() { links.clear(); }
 		
 		size_t size() const { return links.size(); }
@@ -52,14 +62,17 @@ namespace ben {
 		iterator end() { return links.end(); }
 	}; //class LinkManager
 
-	
+	//this alias significantly cleans up the interface, and makes it safer
 	template<typename P>
+	using LinkManager = LinkManagerHelper<P, typename P::construction_types>;
+	
+	template<typename P, typename... ARGS>
 	void LinkManager<P>::clean_up(const id_type address) {
 		auto iter = find(address);
 		if(iter != end()) links.erase(iter);
 	}
 
-	template<typename P>
+	template<typename P, typename... ARGS>
 	typename LinkManager<P>::iterator LinkManager<P>::find(const id_type address) {
 		auto it = begin();
 		for(auto ite=end(); it!=ite; ++it) 
@@ -67,18 +80,18 @@ namespace ben {
 		return it;
 	}
 
-	template<typename P>
-	bool LinkManager<P>::add(const id_type address, const Args... args) {
+	template<typename P, typename... ARGS>
+	bool LinkManager<P>::add(const id_type address, const ARGS... args) { //should add have index access?
 		if( get_index().contains(address) ) {
 			if( contains(address) ) return false;
-			links.push_back( link_type(address, args) );
+			links.push_back( link_type(address, args...) );
 			get_index().elem(address).links.push_back( link_type(links.back(), ID()) );
 			return true;
 		} else return false;
 	}
 
-	template<typename P>
-	bool LinkManager<P>::clone(const self_type& other) {
+	template<typename P, typename... ARGS>
+	bool LinkManager<P>::clone(const self_type& other) { //how does clone know how to copy links?
 	//a way to explicitly copy a set of links, replaces the copy constructor for this purpose
 		if( other.is_managed_by(get_index()) ) { 
 			clear();
@@ -91,19 +104,19 @@ namespace ben {
 		} else return false;
 	}
 
-	template<typename P>
+	template<typename P, typename... ARGS>
 	void remove(const id_type address, complement_type& other) {
 		auto iter = find(address);
 		if(iter != end()) remove(iter, other);
 	}
 
-	template<typename P>
+	template<typename P, typename... ARGS>
 	void LinkManager<P>::remove(const iterator iter, complement_type& other) {
 		auto address = iter->get_address();
 		links.erase(iter);
 		other.clean_up(ID());
-
 	}
+
 
 } //namespace ben
 
